13dfe9359f54966486d92a09d4788e14a43ed4b8
Carlos Alves <carlosaalves07@outlook.com>
2023-03-02 17:11:47 -0300

    feat: new acessory and button for car details page

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+
+  padding: 19px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`;
--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
 import { ImageSlider } from "../../components/ImageSlider";
+import { Acessory } from "../../components/Accessory";
+
+import speedSvg from "../../assets/speed.svg";
+import accelerationSvg from "../../assets/acceleration.svg";
+import forceSvg from "../../assets/force.svg";
+import gasolinaSvg from "../../assets/gasoline.svg";
+import exchangeSvg from "../../assets/exchange.svg";
+import peopleSvg from "../../assets/people.svg";
 
 import {
   Container,
@@ -16,6 +25,7 @@ import {
   Period,
   Price,
   About,
+  Acessories,
 } from "./styles";
 
 export function CarDetails() {
@@ -45,13 +55,21 @@ export function CarDetails() {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
-
+        <Acessories>
+          <Acessory name="380Km/h" icon={speedSvg} />
+          <Acessory name="3.2s" icon={accelerationSvg} />
+          <Acessory name="800 HP" icon={forceSvg} />
+          <Acessory name="Gasolina" icon={gasolinaSvg} />
+          <Acessory name="Auto" icon={exchangeSvg} />
+          <Acessory name="2 pessoas" icon={peopleSvg} />
+        </Acessories>
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide
           indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
           para quem gosta de acelerar.
         </About>
       </Content>
+      <Button title="Escolher periodo do aluguel" />
     </Container>
   );
 }
